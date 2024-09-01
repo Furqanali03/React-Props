cca03160744fd523379a16d6b44808fdbdbb2a42
[Furqanali03] <[alif63503@gmail.com]>
2024-09-02 00:33:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Carousal from './Carousal/Carousal'
+import Cards from './Cards/Cards'
+import Footer from './Footer/Footer'
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar logo="React Props" />
+    <Carousal />
+    <Cards />
+    <Footer logo="React Props" /> 
+  
+
+    </>
+     )
+}
+
+export default App
---
+++ src/Cards/Cards.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <>
+    <div className='flex flex-wrap justify-around mt-8'>
+
+    <div className="card card-side bg-base-100 shadow-xl gap-2 m-4">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      alt="Movie" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
+<div className="card card-side bg-base-100 shadow-xl gap-2 m-4">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      alt="Movie" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
+<div className="card card-side bg-base-100 shadow-xl gap-2 m-4">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      alt="Movie" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
+<div className="card card-side bg-base-100 shadow-xl gap-2 m-4">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      alt="Movie" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
+
+
+    </div>
+
+    </>
+  )
+}
+
+export default Cards
